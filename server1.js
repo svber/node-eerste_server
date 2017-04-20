fdb1b4d85499b541c5e6226a4e72a80fff72cab0
sven berning <srjberni@avans.nl>
2017-04-20 11:42:32 +0200

    veranderd naar hoi

--- server1.js
+++ server1.js
@@ -3,7 +3,7 @@ var http = require('http');
 function onRequest(request, response){
 	console.log('er as een request');
 	response.writeHead(200,{'content-type':'text/html'});
-	response.write('<H1>Hello World!</H1>');
+	response.write('<H1>hoi</H1>');
 	response.end();
 }
 
