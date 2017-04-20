3de24d29b57a09e2a29ed23aebb11675a54ebcaf
sven berning <srjberni@avans.nl>
2017-04-20 11:43:53 +0200

    double

--- server1.js
+++ server1.js
@@ -4,6 +4,7 @@ function onRequest(request, response){
 	console.log('er as een request');
 	response.writeHead(200,{'content-type':'text/html'});
 	response.write('<H1>hoi</H1>');
+	response.write('<H1>hoi2</H1>');
 	response.end();
 }
 
